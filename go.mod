dd587394bd7d37252e8fd50b34b702349f4e4f70
elcomendante <karol@finsane.com>
2022-05-31 18:58:44 +0100

    db conn

--- go.mod
+++ go.mod
@@ -7,4 +7,11 @@ require (
 	github.com/go-chi/render v1.0.1
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/lib/pq v1.10.6
+	gorm.io/gorm v1.23.5
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	gorm.io/driver/mysql v1.3.4
 )
--- internal/db/db.go
+++ internal/db/db.go
@@ -1,42 +1,46 @@
 package db
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 
-	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
+	"gorm.io/driver/mysql"
+	_ "gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-const (
-	HOST = "golang-mysql-1"
-	PORT = "3306"
-)
+var ErrNotFound = fmt.Errorf("record not found")
 
-var ErrNoMatch = fmt.Errorf("no matching record")
+var datetimePrecision = 2
 
-type Database struct {
-	Conn *sql.DB
+type Connection struct {
+	*gorm.DB
 }
 
-func Initialize(username, password, database string) (Database, error) {
-
+func Initialize(userName, password, HOST, PORT, databaseName string) (*Connection, error) {
 	log.Println("Connecting to db")
-	// conn, err := sql.Open("mysql", "user:password@tcp(golang-mysql-1:3306)/database")
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, HOST, PORT, database)
 
-	db := Database{}
-	conn, err := sql.Open("mysql", dsn)
+	DSN := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", userName, password, HOST, PORT, databaseName)
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DSN:                       DSN,                // data source name, refer https://github.com/go-sql-driver/mysql#dsn-data-source-name
+		DefaultStringSize:         256,                // add default size for string fields, by default, will use db type `longtext` for fields without size, not a primary key, no index defined and don't have default values
+		DisableDatetimePrecision:  true,               // disable datetime precision support, which not supported before MySQL 5.6
+		DefaultDatetimePrecision:  &datetimePrecision, // default datetime precision
+		DontSupportRenameIndex:    true,               // drop & create index when rename index, rename index not supported before MySQL 5.7, MariaDB
+		DontSupportRenameColumn:   true,               // use change when rename column, rename rename not supported before MySQL 8, MariaDB
+		SkipInitializeWithVersion: false,              // smart configure based on used version
+	}), &gorm.Config{})
 	if err != nil {
-		return db, err
-	}
-
-	db.Conn = conn
-	err = db.Conn.Ping()
-	if err != nil {
-		return db, err
+		return nil, err
 	}
 	log.Println("Database connection established")
-	return db, nil
+	return &Connection{db}, nil
 }
+
+// _ "github.com/go-sql-driver/mysql"
+
+// const (
+// 	HOST = "golang-mysql-1"
+// 	PORT = "3306"
+// )
